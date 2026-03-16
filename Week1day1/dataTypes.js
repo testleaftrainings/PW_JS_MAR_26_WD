e041881106b35b7e865360ba77bbb96fe717e59e
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-03-17 00:54:43 +0530

    Week1day1
    Week1day1

---
+++ Week1day1/dataTypes.js
@@ -0,0 +1,41 @@
+ 
+ //string
+ var employeeName="16-03-26"  //'',``
+ console.log(employeeName);
+ console.log(typeof employeeName);
+
+ //Number
+ var mobileNumber=7358619.00
+ console.log(mobileNumber);
+ console.log(typeof mobileNumber);
+
+ //Boolean
+ var isPlayright=true
+ console.log(isPlayright);
+ console.log(typeof isPlayright);
+
+ //undefined
+ var empid
+ console.log(empid)
+ console.log(typeof empid);
+ 
+
+//null
+var landLineNumber=null
+console.log(landLineNumber);
+console.log(typeof landLineNumber);
+
+//bigint
+
+var accountNumber=560009876542667290n
+console.log(accountNumber);
+console.log(typeof accountNumber);
+
+
+ 
+ 
+ 
+ 
+
+ 
+ 
